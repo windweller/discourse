ec1014435edfab2cfe29b18de43ea095dc701ea8
Erin Bennett <erindb@stanford.edu>
2017-06-28 15:43:55 -0700

    Revert "more things we won't use..."
    This reverts commit 0c0e160281e434def40bf2fb880a54d5ad2af859.

--- data_processing/process_ptb.py
+++ data_processing/process_ptb.py
@@ -7,7 +7,7 @@ with sentences (or pairs of sentences) that include that discourse marker.
 
 e.g. wsj_BECAUSE.txt
 
-At first, just news sentences. We can extend to more later.
+At first, just WSJ sentences. We can extend to more later.
 
 """
 
@@ -18,27 +18,49 @@ import re
 training_sentences = {"because": [], "but": []}
 discourse_markers = training_sentences.keys()
 
-# initialize loader
 ptb = LazyCorpusLoader( # Penn Treebank v3: WSJ and Brown portions
     'ptb', CategorizedBracketParseCorpusReader,
     r'(wsj/\d\d/wsj_\d\d|brown/c[a-z]/c[a-z])\d\d.mrg',
     cat_file='allcats.txt', tagset='wsj')
 
-# exclude whatever the "-NONE-" tag is...
 def get_words(s):
 	return [pair[0] for pair in s if pair[1] != "-NONE-"]
+	# return [w for w in words if not re.match("\*.*", w)]
 
 print(str(len(ptb.fileids('news'))) + " documents")
 
 num_sentences = 0
-# restrict to news sentences:
 for fileid in ptb.fileids('news'):
 	sentences = ptb.tagged_sents(fileid)
 	num_sentences += len(sentences)
 	for s in sentences:
 		words = get_words(s)
-		for marker in discourse_markers:
-			if marker in words:
-				print " ".join(words)
+		print(" ".join(words))
 
 print(str(num_sentences) + " sentences")
+	
+
+
+# print(ptb.words('mrg/wsj/00/wsj_0010.mrg'))
+
+# from nltk.corpus.reader.tagged import TaggedCorpusReader
+# reader = TaggedCorpusReader("../data/ptb/mrg/wsj/00/", 'wsj_001.*.mrg')
+# sentences = reader.sents()[1:]
+
+# for s in sentences:
+# 	print " ".join(s)
+
+# 	>>> from nltk.corpus import ptb
+# >>> print(ptb.fileids()) # doctest: +SKIP
+# ['BROWN/CF/CF01.MRG', 'BROWN/CF/CF02.MRG', 'BROWN/CF/CF03.MRG', 'BROWN/CF/CF04.MRG', ...]
+# >>> print(ptb.words('WSJ/00/WSJ_0003.MRG')) # doctest: +SKIP
+# ['A', 'form', 'of', 'asbestos', 'once', 'used', '*', ...]
+# >>> print(ptb.tagged_words('WSJ/00/WSJ_0003.MRG')) # doctest: +SKIP
+# [('A', 'DT'), ('form', 'NN'), ('of', 'IN'), ...]
+
+# for each directory in pos_dir
+
+# for each file in that directory
+
+# collect sentences for each discourse marker
+

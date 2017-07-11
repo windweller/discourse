ae19c35eafb8b98c5cfcc53f8f125dc946c322c8
Erin Bennett <erindb@stanford.edu>
2017-07-11 13:40:09 -0700

    choose a set of discourse relations

---
+++ choosing_connectives.R
@@ -0,0 +1,19 @@
+# data from manual:
+# 
+# The PDTB Research Group. 2008.
+# The PDTB 2.0. Annotation Manual. Technical Report IRCS-08-01.
+# Institute for Research in Cognitive Science, University of Pennsylvania.
+
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+total = 18459
+df = read.csv("discourse_connectives_from_ptb.csv",
+              col.names = c("connective", "freq")) %>%
+  mutate(percent = freq/total*100) %>%
+  mutate(connective = factor(connective, levels=connective[order(freq)]))
+
+most_frequent = df %>% filter(percent>=2) %>% select(connective, percent)
+
+print(most_frequent, row.names=F)
+nrow(most_frequent)

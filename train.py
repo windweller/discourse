7898b7ade0cd865445bfe4379e05e07120f7d21a
Erin Bennett <erindb@stanford.edu>
2017-08-01 13:06:05 -0700

    better logic for include/exclude

--- train.py
+++ train.py
@@ -54,13 +54,15 @@ def main(_):
     logging.info("vocab size: {}".format(vocab_size))
 
 
-    if FLAGS.exclude=="" and FLAGS.include=="":
+    if FLAGS.exclude == "" and FLAGS.include == "":
         tag = "all"
-    elif FLAGS.include=="":
+    elif FLAGS.exclude != "":
         tag = "no_" + FLAGS.exclude.replace(",", "_").replace(" ", "_")
         # last part is for "for example"
-    else:
+    elif FLAGS.include != "":
         tag = FLAGS.include.replace(",", "_").replace(" ", "_")
+    else:
+        raise Exception("no match state for exclude/include")
 
 
     pkl_train_name = pjoin("data", FLAGS.dataset, "train_{}.ids.pkl".format(tag))

23c39a04c76e36bf1600404bc7d797375d1c67bd
windweller <leo.niecn@gmail.com>
2017-08-05 15:48:18 -0700

    data gen simple now tokenizes sentences

--- data_gen_simple.py
+++ data_gen_simple.py
@@ -45,20 +45,20 @@ def get_wiki_pairs(file_path):
         for sent in sent_list:
             words = sent.split()  # strip puncts and then split (already tokenized)
             if "but" in words[1:]:  # # no sentence from beginning has but
-                ssplit = sent.split("but")
-                but_sents.append((ssplit[0], ssplit[1]))
+                idx = words.index("but")
+                but_sents.append((words[:idx], words[idx+1:]))
             if "because" in words[1:]:  # no sentence has because at beginning
-                ssplit = sent.split("because")
-                because_sents.append((ssplit[0], ssplit[1]))
+                idx = words.index("because")
+                because_sents.append((words[:idx], words[idx+1:]))
             if "when" in words[1:]:  # exclude "When xxxx, xxx"
-                ssplit = sent.split("when")
-                when_sents.append((ssplit[0], ssplit[1]))
+                idx = words.index("when")
+                when_sents.append((words[:idx], words[idx+1:]))
             if "if" in words[1:]:
-                ssplit = sent.split("if")
-                if_sents.append((ssplit[0], ssplit[1]))  # exclude "If xxx, xxx"
+                idx = words.index("if")
+                if_sents.append((words[:idx], words[idx+1:]))  # exclude "If xxx, xxx"
             if "for example" in sent:  # "for example ..."
-                ssplit = sent.split("for example")
-                for_example_sents.append((ssplit[0], ssplit[1]))
+                idx = words.index("for example")
+                for_example_sents.append((words[:idx], words[idx+1:]))
 
     return but_sents, because_sents, when_sents, if_sents, for_example_sents
 

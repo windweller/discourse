2b015629fea2969e9c2cf2aa777438077245c967
windweller <leo.niecn@gmail.com>
2017-08-11 17:35:58 -0700

    add temporal max pooling

--- classifier.py
+++ classifier.py
@@ -38,6 +38,7 @@ tf.app.flags.DEFINE_string("dataset", "wikitext-103", "ptb/wikitext-103 select t
 tf.app.flags.DEFINE_string("embed_path", "data/wikitext-103/glove.trimmed.300.npz", "Path to the trimmed GLoVe embedding")
 tf.app.flags.DEFINE_string("restore_checkpoint", None, "checkpoint file to restore model parameters from")
 tf.app.flags.DEFINE_boolean("dev", False, "if flag true, will run on dev dataset in a pure testing mode")
+tf.app.flags.DEFINE_boolean("temp_max", False, "if flag true, will use Temporal Max Pooling")
 tf.app.flags.DEFINE_boolean("correct_example", False, "if flag false, will print error, true will print out success")
 tf.app.flags.DEFINE_integer("best_epoch", 1, "enter the best epoch to use")
 tf.app.flags.DEFINE_integer("num_examples", 30, "enter the best epoch to use")
@@ -96,7 +97,8 @@ class Encoder(object):
         cell = DropoutWrapper(cell, input_keep_prob=self.keep_prob, seed=123)
         self.encoder_cell = tf.nn.rnn_cell.MultiRNNCell([cell] * num_layers, state_is_tuple=state_is_tuple)
 
-    def encode(self, inputs, masks, reuse=False, scope_name=""):
+    # could consider instead of averaging, I concatenate
+    def encode(self, inputs, masks, reuse=False, scope_name="", temporal_max_pooling=False):
         """
         In a generalized encode function, you pass in your inputs,
         masks, and an initial
@@ -122,6 +124,7 @@ class Encoder(object):
                 (fw_out, bw_out), (output_state_fw, output_state_bw) = tf.nn.bidirectional_dynamic_rnn(self.encoder_cell,
                                                                                          self.encoder_cell, inp, srclen,
                                                                                          scope=scope, dtype=tf.float32)
+                # (batch_size, T, hidden_size)
                 out = fw_out + bw_out
 
             # before we are using state_is_tuple=True, meaning we only chose top layer
@@ -132,7 +135,12 @@ class Encoder(object):
             else:
                 # last layer [-1], hidden state [1]
                 # this works with multilayer
-                encoder_outputs = tf.add(output_state_fw[-1][1], output_state_bw[-1][1])
+                if temporal_max_pooling:
+                    max_forward = tf.reduce_max(fw_out, axis=1)
+                    max_backward = tf.reduce_max(bw_out, axis=1)
+                    encoder_outputs = max_forward + max_backward
+                else:
+                    encoder_outputs = tf.add(output_state_fw[-1][1], output_state_bw[-1][1])
 
         return out, encoder_outputs
 
@@ -196,8 +204,8 @@ class SequenceClassifier(object):
     def setup_but_because(self):
         # For Erin: this is the MODEL!!!
         # seqA: but, seqB: because, this will learn to differentiate them
-        seqA_w_matrix, seqA_c_vec = self.encoder.encode(self.seqA_inputs, self.seqA_mask)
-        seqB_w_matrix, seqB_c_vec = self.encoder.encode(self.seqB_inputs, self.seqB_mask, reuse=True)
+        seqA_w_matrix, seqA_c_vec = self.encoder.encode(self.seqA_inputs, self.seqA_mask, temp_max=FLAGS.temp_max)
+        seqB_w_matrix, seqB_c_vec = self.encoder.encode(self.seqB_inputs, self.seqB_mask, reuse=True , temp_max=FLAGS.temp_max)
 
         self.seqA_rep = seqA_c_vec
         self.seqB_rep = seqB_c_vec

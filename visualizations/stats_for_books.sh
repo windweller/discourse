334384d4854d97ddedeeef1762df857d4f56e6ac
Erin Bennett <erindb@stanford.edu>
2017-08-16 15:07:51 -0700

    estimate of sentence-internal and sentence-initial examples in book corpus for 18 discourse markers above 1% in PTDB

---
+++ visualizations/stats_for_books.sh
@@ -0,0 +1,90 @@
+marker="after"
+grep -ci "^"$marker"[^a-z]" books_large_p1.txt # init1
+grep -ci "[^a-z]"$marker"[^a-z]" books_large_p1.txt # total1
+grep -ci "^"$marker"[^a-z]" books_large_p2.txt # init2
+grep -ci "[^a-z]"$marker"[^a-z]" books_large_p2.txt # internal2
+marker="also"
+grep -ci "^"$marker"[^a-z]" books_large_p1.txt # init1
+grep -ci "[^a-z]"$marker"[^a-z]" books_large_p1.txt # total1
+grep -ci "^"$marker"[^a-z]" books_large_p2.txt # init2
+grep -ci "[^a-z]"$marker"[^a-z]" books_large_p2.txt # internal2
+marker="although"
+grep -ci "^"$marker"[^a-z]" books_large_p1.txt # init1
+grep -ci "[^a-z]"$marker"[^a-z]" books_large_p1.txt # total1
+grep -ci "^"$marker"[^a-z]" books_large_p2.txt # init2
+grep -ci "[^a-z]"$marker"[^a-z]" books_large_p2.txt # internal2
+marker="and"
+grep -ci "^"$marker"[^a-z]" books_large_p1.txt # init1
+grep -ci "[^a-z]"$marker"[^a-z]" books_large_p1.txt # total1
+grep -ci "^"$marker"[^a-z]" books_large_p2.txt # init2
+grep -ci "[^a-z]"$marker"[^a-z]" books_large_p2.txt # internal2
+marker="as"
+grep -ci "^"$marker"[^a-z]" books_large_p1.txt # init1
+grep -ci "[^a-z]"$marker"[^a-z]" books_large_p1.txt # total1
+grep -ci "^"$marker"[^a-z]" books_large_p2.txt # init2
+grep -ci "[^a-z]"$marker"[^a-z]" books_large_p2.txt # internal2
+marker="because"
+grep -ci "^"$marker"[^a-z]" books_large_p1.txt # init1
+grep -ci "[^a-z]"$marker"[^a-z]" books_large_p1.txt # total1
+grep -ci "^"$marker"[^a-z]" books_large_p2.txt # init2
+grep -ci "[^a-z]"$marker"[^a-z]" books_large_p2.txt # internal2
+marker="before"
+grep -ci "^"$marker"[^a-z]" books_large_p1.txt # init1
+grep -ci "[^a-z]"$marker"[^a-z]" books_large_p1.txt # total1
+grep -ci "^"$marker"[^a-z]" books_large_p2.txt # init2
+grep -ci "[^a-z]"$marker"[^a-z]" books_large_p2.txt # internal2
+marker="but"
+grep -ci "^"$marker"[^a-z]" books_large_p1.txt # init1
+grep -ci "[^a-z]"$marker"[^a-z]" books_large_p1.txt # total1
+grep -ci "^"$marker"[^a-z]" books_large_p2.txt # init2
+grep -ci "[^a-z]"$marker"[^a-z]" books_large_p2.txt # internal2
+marker="for example"
+grep -ci "^for example[^a-z]" books_large_p1.txt # init1
+grep -ci "[^a-z]for example[^a-z]" books_large_p1.txt # total1
+grep -ci "^for example[^a-z]" books_large_p2.txt # init2
+grep -ci "[^a-z]for example[^a-z]" books_large_p2.txt # internal2
+marker="however"
+grep -ci "^"$marker"[^a-z]" books_large_p1.txt # init1
+grep -ci "[^a-z]"$marker"[^a-z]" books_large_p1.txt # total1
+grep -ci "^"$marker"[^a-z]" books_large_p2.txt # init2
+grep -ci "[^a-z]"$marker"[^a-z]" books_large_p2.txt # internal2
+marker="if"
+grep -ci "^"$marker"[^a-z]" books_large_p1.txt # init1
+grep -ci "[^a-z]"$marker"[^a-z]" books_large_p1.txt # total1
+grep -ci "^"$marker"[^a-z]" books_large_p2.txt # init2
+grep -ci "[^a-z]"$marker"[^a-z]" books_large_p2.txt # internal2
+marker="meanwhile"
+grep -ci "^"$marker"[^a-z]" books_large_p1.txt # init1
+grep -ci "[^a-z]"$marker"[^a-z]" books_large_p1.txt # total1
+grep -ci "^"$marker"[^a-z]" books_large_p2.txt # init2
+grep -ci "[^a-z]"$marker"[^a-z]" books_large_p2.txt # internal2
+marker="so"
+grep -ci "^"$marker"[^a-z]" books_large_p1.txt # init1
+grep -ci "[^a-z]"$marker"[^a-z]" books_large_p1.txt # total1
+grep -ci "^"$marker"[^a-z]" books_large_p2.txt # init2
+grep -ci "[^a-z]"$marker"[^a-z]" books_large_p2.txt # internal2
+marker="still"
+grep -ci "^"$marker"[^a-z]" books_large_p1.txt # init1
+grep -ci "[^a-z]"$marker"[^a-z]" books_large_p1.txt # total1
+grep -ci "^"$marker"[^a-z]" books_large_p2.txt # init2
+grep -ci "[^a-z]"$marker"[^a-z]" books_large_p2.txt # internal2
+marker="then"
+grep -ci "^"$marker"[^a-z]" books_large_p1.txt # init1
+grep -ci "[^a-z]"$marker"[^a-z]" books_large_p1.txt # total1
+grep -ci "^"$marker"[^a-z]" books_large_p2.txt # init2
+grep -ci "[^a-z]"$marker"[^a-z]" books_large_p2.txt # internal2
+marker="though"
+grep -ci "^"$marker"[^a-z]" books_large_p1.txt # init1
+grep -ci "[^a-z]"$marker"[^a-z]" books_large_p1.txt # total1
+grep -ci "^"$marker"[^a-z]" books_large_p2.txt # init2
+grep -ci "[^a-z]"$marker"[^a-z]" books_large_p2.txt # internal2
+marker="when"
+grep -ci "^"$marker"[^a-z]" books_large_p1.txt # init1
+grep -ci "[^a-z]"$marker"[^a-z]" books_large_p1.txt # total1
+grep -ci "^"$marker"[^a-z]" books_large_p2.txt # init2
+grep -ci "[^a-z]"$marker"[^a-z]" books_large_p2.txt # internal2
+marker="while"
+grep -ci "^"$marker"[^a-z]" books_large_p1.txt # init1
+grep -ci "[^a-z]"$marker"[^a-z]" books_large_p1.txt # total1
+grep -ci "^"$marker"[^a-z]" books_large_p2.txt # init2
+grep -ci "[^a-z]"$marker"[^a-z]" books_large_p2.txt # internal2

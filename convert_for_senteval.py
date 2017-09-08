d2f679cfbd584dbb6afc613ba300eea7665b5bb0
Erin Bennett <erindb@stanford.edu>
2017-09-08 02:09:14 -0700

    switch senteval task to wikitext BOOKS5

--- convert_for_senteval.py
+++ convert_for_senteval.py
@@ -41,41 +41,60 @@ def initialize_vocabulary(vocabulary_path):
 
 def load_data(rev_vocab, rev_labels):
 	print("(books)")
-	old_data_test = pickle.load(open("data/books/test_but_because_when_if_for_example_so_before_still.ids.pkl", "rb"))
-	old_data_val = pickle.load(open("data/books/valid_but_because_when_if_for_example_so_before_still.ids.pkl", "rb"))
+	# old_data_test = pickle.load(open("data/books/test_but_because_when_if_for_example_so_before_still.ids.pkl", "rb"))
+	# old_data_val = pickle.load(open("data/books/valid_but_because_when_if_for_example_so_before_still.ids.pkl", "rb"))
 
 	# get for example from wikitext
 	print("(wiki)")
-	wiki_data = pickle.load(open("data/wikitext-103/dep_all_sentence_pairs.pkl", "rb"))
+	# wiki_data = pickle.load(open("data/wikitext-103/dep_all_sentence_pairs.pkl", "rb"))
+	wiki_data = pickle.load(open("data/wikitext-103/wikitext-103_all_sentence_pairs.pkl", "rb"))
 
 	new_data = []
-	for s1, s2 in wiki_data["for example"]:
-		marker = "for example"
-		words1 = " ".join(s1)
-		words2 = " ".join(s2)
-		new_data.append((words1, words2, marker))
+	max_pairs = 1500
+	for marker in ["but", "because", "if", "when", "so"]:
+	# for s1, s2 in wiki_data["for example"]:
+		n_pairs = 0
+		for s1, s2 in wiki_data[marker]:
+			n_pairs += 1
+			if n_pairs < 10:
+				print(" ".join(s1))
+				print(marker.upper())
+				print(" ".join(s2))
+				print("***********")
+			if n_pairs > max_pairs:
+				break
+			# marker = "for example"
+			words1 = " ".join(s1)
+			words2 = " ".join(s2)
+			new_data.append((words1, words2, marker))
+		print("***********")
+		print("***********")
+		print("***********")
+		print("***********")
 
-	old_data = old_data_test #+ old_data_val
-	for s1, s2, label in old_data:
-		marker = rev_labels[label]
-		# if marker != "for example":
-		words1 = " ".join([rev_vocab[i] for i in s1])
-		words2 = " ".join([rev_vocab[i] for i in s2])
-		new_data.append((words1, words2, marker))
+	# old_data = old_data_test #+ old_data_val
+	# for s1, s2, label in old_data:
+	# 	marker = rev_labels[label]
+	# 	# if marker != "for example":
+	# 	words1 = " ".join([rev_vocab[i] for i in s1])
+	# 	words2 = " ".join([rev_vocab[i] for i in s2])
+	# 	new_data.append((words1, words2, marker))
 
 	return new_data
 
 
 if __name__ == '__main__':
 
-	class_labels = pickle.load(open("data/books/class_labels_but_because_when_if_for_example_so_before_still.pkl", "rb"))
-	rev_labels = [marker for marker in class_labels]
-	for marker in class_labels:
-	    label = class_labels[marker]
-	    rev_labels[label] = marker
+	# class_labels = pickle.load(open("data/books/class_labels_but_because_when_if_for_example_so_before_still.pkl", "rb"))
+	# rev_labels = [marker for marker in class_labels]
+	# for marker in class_labels:
+	#     label = class_labels[marker]
+	#     rev_labels[label] = marker
 
-	print("vocab")
-	vocab, rev_vocab = initialize_vocabulary( "data/books/vocab_but_because_when_if_for_example_so_before_still.dat")
+	# print("vocab")
+	# vocab, rev_vocab = initialize_vocabulary( "data/books/vocab_but_because_when_if_for_example_so_before_still.dat")
+	rev_vocab = None
+	rev_labels = None
 
 	print("load data")
 	full_dataset = load_data(rev_vocab, rev_labels)
@@ -114,7 +133,7 @@ if __name__ == '__main__':
 		print(split)
 		for element in elements:
 			print(element)
-			open("data/books/discourse_task_v1/{}.{}".format(element, split), "w").write("\n".join(new_data[split][element]))
+			open("data/wikitext-103/discourse_task_v1/{}.{}".format(element, split), "w").write("\n".join(new_data[split][element]))
 
 	for split in splits:
 		labels = new_data[split]["labels"]

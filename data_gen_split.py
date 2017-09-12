7fab6d8a1282c20e0d41f595dac334162696d15f
Erin Bennett <erindb@stanford.edu>
2017-09-12 15:33:54 -0700

    add conversion to master data script

--- data_gen_split.py
+++ data_gen_split.py
@@ -49,7 +49,125 @@ def setup_args():
     parser.add_argument("--method", default="string_ssplit_int_init", type=str)
     parser.add_argument("--caching", action='store_true')
     parser.add_argument("--action", default='collect_raw', type=str)
+    parser.add_argument("--glove_dim", default=300, type=int)
+    parser.add_argument("--random_init", action='store_true')
     return parser.parse_args()
+
+def initialize_vocabulary(vocabulary_path):
+    # map vocab to word embeddings
+    if gfile.Exists(vocabulary_path):
+        rev_vocab = []
+        with gfile.GFile(vocabulary_path, mode="rb") as f:
+            rev_vocab.extend(f.readlines())
+        rev_vocab = [line.strip('\n') for line in rev_vocab]
+        vocab = dict([(x, y) for (y, x) in enumerate(rev_vocab)])
+        return vocab, rev_vocab
+    else:
+        raise ValueError("Vocabulary file %s not found.", vocabulary_path)
+
+def process_glove(args, vocab_dict, save_path, random_init=True):
+    """
+    :param vocab_list: [vocab]
+    :return:
+    """
+    if gfile.Exists(save_path + ".npz"):
+        print("Glove file already exists at %s" % (save_path + ".npz"))
+    else:
+        glove_path = os.path.join(args.glove_dir, "glove.840B.{}d.txt".format(args.glove_dim))
+        if random_init:
+            glove = np.random.randn(len(vocab_dict), args.glove_dim)
+        else:
+            glove = np.zeros((len(vocab_dict), args.glove_dim))
+
+        found = 0
+
+        with open(glove_path, 'r') as f:
+            for line in f:
+                word, vec = line.split(' ', 1)
+                if word in vocab_dict:  # all cased
+                    idx = vocab_dict[word]
+                    glove[idx, :] = np.fromstring(vec, sep=' ')
+                    found += 1
+
+        print("{}/{} of word vocab have corresponding vectors in {}".format(found, len(vocab), glove_path))
+        np.savez_compressed(save_path, glove=glove)
+        print("saved trimmed glove matrix at: {}".format(save_path))
+
+
+def create_vocabulary(vocabulary_path, sentence_pairs_data, discourse_markers=None):
+    if gfile.Exists(vocabulary_path):
+        print("Vocabulary file already exists at %s" % vocabulary_path)
+    else:
+        print("Creating vocabulary {}".format(vocabulary_path))
+        vocab = {}
+        counter = 0
+
+        for s1, s2, label in sentence_pairs_data:
+            counter += 1
+            if counter % 100000 == 0:
+                print("processing line %d" % counter)
+            for w in s1:
+                if not w in _START_VOCAB:
+                    if w in vocab:
+                        vocab[w] += 1
+                    else:
+                        vocab[w] = 1
+            for w in s2:
+                if not w in _START_VOCAB:
+                    if w in vocab:
+                        vocab[w] += 1
+                    else:
+                        vocab[w] = 1
+
+        vocab_list = _START_VOCAB + sorted(vocab, key=vocab.get, reverse=True)
+        print("Vocabulary size: %d" % len(vocab_list))
+        with gfile.GFile(vocabulary_path, mode="wb") as vocab_file:
+            for w in vocab_list:
+                vocab_file.write(w + b"\n")
+
+
+def sentence_to_token_ids(sentence, vocabulary):
+    return [vocabulary.get(w, UNK_ID) for w in sentence]
+
+
+def merge_dict(dict_list1, dict_list2):
+    for key, list_sent in dict_list1.iteritems():
+        dict_list1[key].extend(dict_list2[key])
+    return dict_list1
+
+def data_to_token_ids(data, all_labels, class_label_dict, target_path, text_path, vocabulary_path, data_dir):
+    rev_class_labels = all_labels
+    if gfile.Exists(target_path):
+        print("file {} already exists".format(target_path))
+    else:
+        vocab, _ = initialize_vocabulary(vocabulary_path)
+
+        ids_data = []
+        text_data = []
+
+        counter = 0
+        for s1, s2, text_label in data:
+            label = class_label_dict[text_label]
+            counter += 1
+            if counter % 100000 == 0:
+                print("converting %d" % (counter))
+            token_ids_s1 = sentence_to_token_ids(s1, vocab)
+            token_ids_s2 = sentence_to_token_ids(s2, vocab)
+            ids_data.append((token_ids_s1, token_ids_s2, label))
+            text_data.append((s1, s2, label))
+
+        shuffled_idx = range(len(ids_data))
+        random.shuffle(shuffled_idx)
+        shuffled_ids_data = [ids_data[idx] for idx in shuffled_idx]
+        shuffled_text_data = [text_data[idx] for idx in shuffled_idx]
+
+        print("writing {} and {}".format(target_path, text_path))
+        pickle.dump(shuffled_ids_data, gfile.GFile(target_path, mode="wb"))
+
+        with gfile.GFile(text_path, mode="wb") as f:
+            for t in shuffled_text_data:
+                f.write(str([" ".join(t[0]), " ".join(t[1]), rev_class_labels[t[2]]]) + "\n")
+
 
 def undo_rephrase(lst):
     return " ".join(lst).replace("for_example", "for example").split()
@@ -152,7 +270,7 @@ def collect_raw_sentences(source_dir, dataset, caching):
                 else:
                     proxy_marker = marker
 
-                if proxy_marker in words:
+                if proxy_marker in [w.lower() for w in words]:
                     sentences[marker]["sentence"].append(sentence)
                     sentences[marker]["previous"].append(previous_sentence)
             previous_sentence = sentence
@@ -252,6 +370,72 @@ def ssplit(method, source_dir, data_tag, train_size):
                     element = splits[split][element_type][index]
                     write_file.write(element + "\n")
 
+def conversion(method, source_dir, data_tag, train_size, glove_dim, random_init):
+
+    sub_directory = "{}_train{}{}".format(
+        args.method,
+        train_size,
+        extra_tag
+    )
+
+    vocab_path = pjoin(sub_directory, "vocab.dat")
+
+    splits = {
+        "train": [],
+        "valid": [],
+        "test": []
+    }
+
+    def get_file_path(split, element_type):
+        filename = sub_directory + "_{}_{}.txt".format(split, element_type)
+        return pjoin(source_dir, sub_directory, filename)
+
+    for split in splits:
+        s1_path = get_file_path(split, "s1")
+        s2_path = get_file_path(split, "s2")
+        labels_path = get_file_path(split, "label")
+        with open(s1_path) as f1, open(s2_path) as f2, open(labels_path) as flab: 
+            for s1, s2, label in izip(f1, f2, flab):
+                s1 = s1.strip().split()
+                s2 = s2.strip().split()
+                label = label.strip()
+                # if label in all_labels:
+                splits[split].append((s1, s2, label))
+
+    all_examples = splits["train"] + splits["valid"] + splits["test"]
+
+    create_vocabulary(vocab_path, all_examples)
+
+    output_dir = pjoin(source_dir, sub_directory)
+
+    vocab, rev_vocab = initialize_vocabulary(pjoin(output_dir, "vocab.dat"))
+
+    # ======== Trim Distributed Word Representation =======
+    # If you use other word representations, you should change the code below
+
+    process_glove(args, vocab, pjoin(output_dir, "glove.trimmed.{}.npz".format(args.glove_dim)),
+                  random_init=args.random_init)
+
+
+    class_labels = {all_labels[i]: i for i in range(len(all_labels))}
+
+    pickle.dump(class_labels, open(pjoin(output_dir, "class_labels.pkl"), "wb"))
+    pickle.dump(reverse_class_labels, open(pjoin(output_dir, "reverse_class_labels.pkl"), "wb"))
+
+    for split in splits:
+        data = splits[split]
+        print("Converting data in {}".format(split))
+        ids_path = pjoin(
+            args.run_dir,
+            "{}.ids.pkl".format(split)
+        )
+        text_path = pjoin(
+            args.run_dir,
+            "{}.text.txt".format(split)
+        )
+
+        data_to_token_ids(data, all_labels, class_labels, ids_path, text_path, vocab_path, args.run_dir)
+
 
 if __name__ == '__main__':
     args = setup_args()
@@ -264,5 +448,7 @@ if __name__ == '__main__':
         split_raw(source_dir, args.train_size)
     elif args.action == "ssplit":
         ssplit(args.method, source_dir, args.data_tag, args.train_size)
+    elif args.action == "convert":
+        convert(args.method, source_dir, args.data_tag, args.train_size, args.glove_dim, args.random_init)
 
 
